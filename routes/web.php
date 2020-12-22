e7958558e10379de074a8776fb7bb90bfcf298c1
JHONNY25 <jonas97isk@hotmail.com>
2020-12-21 19:14:06 -0700

    add routes and set in group middlewhare

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,4 +20,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {
+    Route::get('/home', [TaskController::class, 'index'])->name('home');
+    Route::get('/get-task', [TaskController::class, 'getTask'])->name('getTask');
+});
