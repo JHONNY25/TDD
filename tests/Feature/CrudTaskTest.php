05ceaff81ac86e42ac9e3eb15afe73939ac55983
JHONNY25 <jonas97isk@hotmail.com>
2020-12-21 22:02:44 -0700

    test for create task

--- tests/Feature/CrudTaskTest.php
+++ tests/Feature/CrudTaskTest.php
@@ -3,10 +3,13 @@
 namespace Tests\Feature;
 
 use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class CrudTaskTest extends TestCase
 {
+    use RefreshDatabase;
+
     /** @test */
     function testGetTask()
     {
@@ -16,5 +19,23 @@ class CrudTaskTest extends TestCase
         $response = $this->actingAs($user)->get('/get-task');
 
         $response->assertOk();
+    }
+
+    /** @test */
+    function testCreateTask()
+    {
+        $this->withoutExceptionHandling();
+
+        $user = User::factory()->create();
+        $asignTo = User::factory()->create();
+
+        $response = $this->actingAs($user)->post('/save-task',[
+            'name' => 'Task one',
+            'description' => 'description test',
+            'user' => $asignTo->id
+        ],['X-CSRF-TOKEN' => csrf_token()]);
+
+        $response->assertStatus(302);
+        $response->assertSessionHas(['success']);
     }
 }
