2aa12e13aa446fad003e38d895a51ea86d1860e8
JHONNY25 <jonas97isk@hotmail.com>
2020-12-21 21:57:01 -0700

    add function for display create and add function for create task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers;
 
 use App\Models\Task;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -16,7 +18,37 @@ class TaskController extends Controller
     public function index(){
         return view('home')->with('tasks',$this->getTask());
     }
+
     public function getTask(){
         return $this->task->with('user')->paginate(3);
+    }
+
+    public function create(){
+        return view('Task.create')->with('users',User::all());
+    }
+
+    public function store(Request $request){
+        $validator = Validator::make($request->all(),[
+            'name' => 'required|max:250',
+            'description' => 'required|max:250',
+            'user' => 'required'
+        ],[
+            '*.required' => 'El campo es requerido',
+            '*.max' => 'El campo acepta solo :max caracteres',
+        ]);
+
+        if ($validator->fails()) {
+            return back()
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        Task::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'user_id' => $request->user
+        ]);
+
+        return redirect()->back()->with('success','Se creo la tarea correctamente.');
     }
 }
