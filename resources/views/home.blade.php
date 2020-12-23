121e15376ec4abbeddda704b2e74dac9f58c907f
JHONNY25 <jonas97isk@hotmail.com>
2020-12-22 18:54:27 -0700

    add button for delete and message success

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-10">
+            @if ($message = Session::get('success'))
+                <div class="alert alert-success alert-block">
+                    <button type="button" class="close" data-dismiss="alert">×</button>
+                    <strong>{{ $message }}</strong>
+                </div>
+            @endif
             <a href="{{ route('createTask') }}" type="button" class="btn btn-success mb-3">Crear tarea</a>
 
             <table class="table table-striped">
@@ -22,8 +28,13 @@
                             <td>{{ $task->name }}</td>
                             <td>{{ $task->description }}</td>
                             <td>{{ $task->user->name }}</td>
-                            <td>
-                                <a href="{{ route('viewUpdateTask',['id' => $task->id]) }}" type="button" class="btn btn-primary">Editar</a>
+                            <td class="d-flex">
+                                <a href="{{ route('viewUpdateTask',['id' => $task->id]) }}" type="button" class="btn btn-sm btn-primary" style="width: 100px;">Editar</a>
+                                <form action="{{ route('removeTask') }}" method="POST">
+                                    @csrf
+                                    <input type="hidden" value="{{ $task->id }}" name="id">
+                                    <button type="submit" class="btn btn-sm btn-danger ml-2" style="width: 100px;">Eliminar tarea</button>
+                                </form>
                             </td>
                         </tr>
                     @endforeach
