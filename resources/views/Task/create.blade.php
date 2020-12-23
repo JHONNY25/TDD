13c76a70ce8f9a738eaabecfcc91e7ef1d4e60da
JHONNY25 <jonas97isk@hotmail.com>
2020-12-22 18:11:32 -0700

    add conditions for update task in same view create

--- resources/views/Task/create.blade.php
+++ resources/views/Task/create.blade.php
@@ -10,18 +10,18 @@
                     <strong>{{ $message }}</strong>
                 </div>
             @endif
-            <form action="{{ route('saveTask') }}" method="POST">
+            <form action="{{ isset($task) ? route('updateTask') : route('saveTask') }}" method="POST">
                 @csrf
                 <div class="form-group">
                   <label for="name">Nombre</label>
-                  <input name="name" type="text" class="form-control" id="name" placeholder="Ingresa el nombre de la tarea">
+                  <input value="{{ isset($task) ? $task->name : '' }}" name="name" type="text" class="form-control" id="name" placeholder="Ingresa el nombre de la tarea">
                 </div>
                 @error('name')
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
                 <div class="form-group">
                     <label for="description">Descripción</label>
-                    <input name="description" type="text" class="form-control" id="description" placeholder="Ingresa la descripción de la tarea">
+                    <input value="{{ isset($task) ? $task->description : '' }}" name="description" type="text" class="form-control" id="description" placeholder="Ingresa la descripción de la tarea">
                 </div>
                 @error('description')
                     <div class="alert alert-danger">{{ $message }}</div>
@@ -29,6 +29,9 @@
                 <div class="form-group">
                   <label for="user">Asigar a :</label>
                   <select name="user" class="form-control" id="user">
+                      @if (isset($task))
+                        <option selected value="{{ $task->user->id }}">{{ $task->user->name }}</option>
+                      @endif
                       @foreach ($users as $user)
                         <option value="{{ $user->id }}">{{ $user->name }}</option>
                       @endforeach
@@ -38,7 +41,9 @@
                     <div class="alert alert-danger">{{ $message }}</div>
                 @enderror
 
-                <button type="submit" class="w-100 btn btn-primary">Crear</button>
+                <input value="{{ isset($task) ? $task->id : '' }}" name="id" type="hidden">
+
+                <button type="submit" class="w-100 btn btn-primary">{{ isset($task) ? 'Actualizar' : 'Crear' }}</button>
               </form>
         </div>
     </div>
